e0742937cdd285dc0156817951c419446817e985
Michelle He <mhe@pivotal.io>
2017-10-16 22:13:15 -0700

    jest test actualy gallery component
    [#151869909] setup CI env

--- client/__tests__/gallery.test.js
+++ client/__tests__/gallery.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Gallery from '../src/components/gallery/gallery';
+import { Gallery } from '../src/components/gallery/gallery';
 import renderer from 'react-test-renderer';
 
 test('Gallery component renders the text inside it', () => {
   const component = renderer.create(
-    <div className="gallery-container">
-      Gallery
-    </div>
+    <Gallery />
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
