1801dfc7c369141ca0c2cdf13845177653f01f6f
jon arnaldo <jonarnaldo@gmail.com>
2017-10-17 20:33:07 -0700

    removes items

--- server/models/items.js
+++ server/models/items.js
@@ -1,31 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    title:  { type: DataTypes.STRING },
-    status: { type: DataTypes.STRING },
-    lat:    { type: DataTypes.DECIMAL },
-    lon:    { type: DataTypes.DECIMAL }
-  });
-
-  // see documentation here: http://docs.sequelizejs.com/manual/tutorial/upgrade-to-v4.html
-  // class methods
-  Post.associate = (models) => {
-    Post.belongsTo(models.User, {
-      foreignKey: 'creator_id'
-    });
-
-
-  }
-
-  // instance methods
-  Post.prototype.getTitle = () => {
-    console.log(this.title);
-  }
-
-  return Post;
+  // const Items = sequelize.define('Post', {
+  //   id: {
+  //     type: DataTypes.INTEGER,
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //     allowNull: false
+  //   },
+  //   title:  { type: DataTypes.STRING },
+  //   status: { type: DataTypes.STRING },
+  //   lat:    { type: DataTypes.DECIMAL },
+  //   lon:    { type: DataTypes.DECIMAL }
+  // });
+  //
+  // // see documentation here: http://docs.sequelizejs.com/manual/tutorial/upgrade-to-v4.html
+  // // class methods
+  // Post.associate = (models) => {
+  //   Post.belongsTo(models.User, {
+  //     foreignKey: 'creator_id'
+  //   });
+  //
+  //
+  // }
+  //
+  // // instance methods
+  // Post.prototype.getTitle = () => {
+  //   console.log(this.title);
+  // }
+  //
+  // return Post;
 }
