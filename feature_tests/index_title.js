35d728e17b487fae4bdf7ba2c1f43e62f88da26a
Michelle He <mhe@pivotal.io>
2017-10-17 20:43:33 -0700

    Add feature testing to the app
    [#152061620]

---
+++ feature_tests/index_title.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('ISO App', () => {
+  it('Should load with the right title', () => {
+    browser.url('http://localhost:3000/');
+    const actualTitle = browser.getTitle();
+
+    expect(actualTitle).to.eql('ISO');
+  });
+});
