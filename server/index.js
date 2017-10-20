a9f9714c0e0f322ec54e6f48bce53e105165b631
Michelle He <mhe@pivotal.io>
2017-10-19 18:53:38 -0700

    close db connection after sync
    [#151869909] setup CI env

--- server/index.js
+++ server/index.js
@@ -2,21 +2,22 @@ const models = require('./models');
 
 console.log("models sunc", models)
 models.sequelize.sync({force:true}).then(function() {
-   models.User.create({
-     email:      "jon@gmail.com",
-     firstname:  "jon",
-     lastname:   "arnaldo",
-     username:   "jonarnaldo",
-     rating:     5,
-     lat:        100.1,
-     lon:        200.2
-   })
+   // models.User.create({
+   //   email:      "jon@gmail.com",
+   //   firstname:  "jon",
+   //   lastname:   "arnaldo",
+   //   username:   "jonarnaldo",
+   //   rating:     5,
+   //   lat:        100.1,
+   //   lon:        200.2
+   // });
 
-   models.Post.create({
-     title:  "a post",
-     status: "CREATED",
-     lat:    "100.1",
-     lon:    "200.2",
-     creator_id: 1
-   })
+   // models.Post.create({
+   //   title:  "a post",
+   //   status: "CREATED",
+   //   lat:    "100.1",
+   //   lon:    "200.2",
+   //   creator_id: 1
+   // });
+   models.sequelize.close();
 });
